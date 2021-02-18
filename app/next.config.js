e404f67d399ff8e2898bb58b2631d03b4717b439
Ido Shamun <idoesh1@gmail.com>
2021-02-18 18:07:42 +0200

    build(app): set dest property for pwa

--- app/next.config.js
+++ app/next.config.js
@@ -6,6 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
   ...withPreact(
     withBundleAnalyzer({
       webpack: (config) => {
